43934abfe3a3ea5396ba23e0a821903eb3ecf775
Tristan Jet <tristanjetlay@gmail.com>
2023-12-31 05:42:13 +0200

    Style change NOT working, will try chatgpt

--- src/components/QuoteDisplay.jsx
+++ src/components/QuoteDisplay.jsx
@@ -1,18 +1,23 @@
 import { useEffect, useState } from "react";
 
 export function QuoteDisplay() {
-  const quote = "Theory can only take you so far.";
+  const quote = "Theory can only take you so far.".split("");
 
   const [inputState, setInputState] = useState([]);
+  const [correctState, setCorrectState] = useState([]);
 
   useEffect(() => {
-
     const charRegex = /[a-z0-9]/i;
 
-    const specialKeys = ["Backspace", " ", ".", ",", "?"];
+    const specialKeys = [" ", ".", ",", "?"];
 
     const handleKeyDown = (event) => {
-      if ((event.key.length === 1 && charRegex.test(event.key)) || specialKeys.includes(event.key)) {
+      if (event.key === "Backspace") {
+        setInputState((prevInputState) => prevInputState.slice(0, -1));
+      } else if (
+        (event.key.length === 1 && charRegex.test(event.key)) ||
+        specialKeys.includes(event.key)
+      ) {
         setInputState((prevInputState) => [...prevInputState, event.key]);
       }
     };
@@ -25,13 +30,28 @@ export function QuoteDisplay() {
   }, []);
 
   useEffect(() => {
-    console.log(inputState);
+    
+    inputState.forEach((inputChar, index) => {
+      if (inputChar === quote[index]) {
+        setCorrectState((prevCorrectState) => [...prevCorrectState, "correct"]);
+      } else {
+        setCorrectState((prevCorrectState) => [
+          ...prevCorrectState,
+          "incorrect",
+        ]);
+      }
+    });
   }, [inputState]);
 
   return (
     <div className="quote-display" id="quoteDisplay">
-      {quote.split("").map((character, index) => (
-        <span key={index}>{character}</span>
+      {quote.map((character, index) => (
+        <span
+          key={index}
+          class={correctState[index] ? correctState[index] : "untyped"}
+        >
+          {character}
+        </span>
       ))}
     </div>
   );
