4750af498abe78a8541490d0cd500a0f293234b4
Tristan Jet <tristanjetlay@gmail.com>
2024-01-06 07:33:57 +0200

    cookie is being correctly set

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App.jsx'
 import './index.css'
 
+const sessionId = import.meta.env.VITE_SESH
+console.log(sessionId)
+document.cookie = `jet-session=${sessionId}; path=/; Secure; SameSite=None`;
+console.log(`Cookie set:  ${document.cookie}`);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
